001108b457e3a6e94ff4e0488f1439f713c9c61a
Caleb Petrus <calebpetrus@gmail.com>
2021-04-19 16:42:59 +0200

    Manifest Version 3
    Cleaned up the code and commented out Console.logs

--- UG_solfege/content_script.js
+++ UG_solfege/content_script.js
@@ -57,20 +57,22 @@ solfege2 = [
   "Si",
 ];
 let cpchord = document.querySelectorAll("[data-name]");
-let cptestchord = "G/D";
+// let cptestchord = "G/D";
+
 // Logic
 // querySelect all chords and "for" loop all of them
 // if chord has slash, split it and transpose each side, then add the slash back in, write the new chord to the webpage
 // else run Transpose on the chord, Write to Webpage
+
 // Transpose logic
 // if chord is found in chords arr, return the solfege equvelent. This Handles Basic Chords
-// if chord is undefined slice the first character, find solfege name then add ending. This Handles Sharps and Flats
-// if chord is still undefined slice 2 charactersfind solfege name then add ending. This Handles eveything else, e.g m7b5, sus4, add9, maj7, dim6susb13no5
+// if chord is undefined and dosent include # or b slice the first character, find solfege name then add ending. This Handles all single character chords
+// if chord is still undefined slice 2 characters, find solfege name then add ending. This Handles eveything else, e.g G#7, m7b5, dim6susb13no5
 
 chrome.runtime.onMessage.addListener(gotMessage);
 
 //Function to recive button click from popup
-function gotMessage(message, sender, sendResponce) {
+function gotMessage(message) {
   console.log(message.txt);
   if (message.txt === "Run") {
     //Start Searching for chords
@@ -97,22 +99,22 @@ function transpose(x) {
     let newindex = chords.indexOf(x);
     let newchord = solfege[newindex];
     console.log(newchord);
-    console.log("if 1 was done");
+    // console.log("if 1 was done");
     return newchord;
   } else if (chords.indexOf(x) == -1 && !(x.includes("b") || x.includes("#"))) {
     let splitchord = x.slice(0, 1);
     let newindex = chords.indexOf(splitchord);
-    console.log(newindex);
+    // console.log(newindex);
     let newchord = solfege[newindex] + x.slice(1);
     console.log(newchord);
-    console.log("if 2 was done");
+    // console.log("if 2 was done");
     return newchord;
   } else if (x.length >= 2) {
     let splitchord = x.slice(0, 2);
     let newindex = chords.indexOf(splitchord);
     let newchord = solfege[newindex] + x.slice(2);
     console.log(newchord);
-    console.log("if 3 was done");
+    // console.log("if 3 was done");
     return newchord;
   }
 }
